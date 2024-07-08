3c9b91128379f0cadfd1859df7ac6f903fc2903d
TImur <kamilyanov_timur@mail.ru>
2024-07-08 21:00:22 +0300

    hw48

---
+++ pydeck/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from blog import views
+
+urlpatterns = [
+    path("", views.index, name="main"),
+    path("admin/", admin.site.urls),
+    path("about/", views.about, name="about"),
+    path("blog/", include("blog.urls")),
+]
--- python_blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import include, path
-from python_blog import views
-
-
-urlpatterns = [
-    path("", views.blog_catalog),
-    path("category/", views.category_list),
-    path("category/<int:category_id>/", views.category_detail),
-]
